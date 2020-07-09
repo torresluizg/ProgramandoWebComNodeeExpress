5310ef1926ef413d0212be91ebd951d26a093c18
Luiz Gustavo Torres <torresluizgtech@gmail.com>
2020-07-09 01:30:13 -0300

    Installing nodemon

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
+const app = express();
+
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}))
+
+app.set('view engine', 'handlebars')
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.render('home'))
+
+app.get('/about', (req, res) => res.render('about'))
+
+app.use((req, res) => {
+    res.status(404)
+    res.render('404')
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.message)
+    res.status(500)
+    res.render('500')
+})
+
+app.listen(port, () => console.log(
+    `Express started on http://localhost:${port}; ` +
+    `press ctrl-c to terminate.`
+))
